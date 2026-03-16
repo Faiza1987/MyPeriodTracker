dfb8946bfc93b116ddee7420a4508f1152a9c6b8
Faiza Ahsan <faiza.ahsan1222@gmail.com>
2026-03-16 14:11:56 -0700

    Update API URL to Render deployment

--- MyPeriodTrackerMobile/config/api.ts
+++ MyPeriodTrackerMobile/config/api.ts
@@ -1,2 +1,2 @@
-export const API_BASE_URL = 'http://192.168.0.197:8080';
+export const API_BASE_URL = 'https://myperiodtracker-xxxx.onrender.com';
 export const USER_ID = '00000000-0000-0000-0000-000000000001';
